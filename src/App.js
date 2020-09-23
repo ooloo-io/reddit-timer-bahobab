973e2ad0550398d27ad1639983499c2ec6ed7ae3
mono motapa <bahobab@hotmail.com>
2020-09-23 13:24:00 -0500

    create app skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
+import './App.css';
+
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <header className="navbar">
+      <div className="logo">
+        <a href="/"><img src="./img/me-avatar.png" alt="logo"/></a>
+      </div>
+      <div className="menu">
+        <nav>
+          <ul className="menu">
+            <a href="/about">
+              <li>About</li>
+            </a>
+            <a href="/search">
+              <li>Search</li>
+            </a>
+          </ul>
+        </nav>
+      </div>
+    </header>
   );
 }
 
