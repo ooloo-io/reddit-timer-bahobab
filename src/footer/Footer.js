a462336eb0322dc0335f6db90873965754386d9d
mono motapa <bahobab@hotmail.com>
2020-09-28 12:18:12 -0500

    implemented Footer.js and Footer.style.js

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,8 +1,19 @@
 import React from 'react';
+// import { Link } from 'react-router-dom';
+
+import { Container, FooterLink, Sign } from './Footer.style';
 
 function Footer() {
   return (
-    <div>Footer placeholder</div>
+    <Container>
+      <FooterLink to="https://ooloo.io/employers">ooloo.io</FooterLink>
+      <FooterLink to="/">
+        <Sign />
+      </FooterLink>
+      <FooterLink to="/terms">
+        Terms & Privacy
+      </FooterLink>
+    </Container>
   );
 }
 
