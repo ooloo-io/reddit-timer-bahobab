f2c2e2dee628a60c96bd1e71e2790ab13112763b
mono motapa <bahobab@hotmail.com>
2020-10-01 17:15:34 -0500

    subreddit-form: implement tests

--- src/__tests__/SearchPage.js
+++ src/__tests__/SearchPage.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { MemoryRouter, Route } from 'react-router-dom';
-import { render } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom/extend-expect';
 
 import App from '../app';
 
-const setup = (initialPath = '/search/javascript') => {
+const setup = (initialPath = '/') => {
   let history;
 
   render(
@@ -25,9 +25,44 @@ const setup = (initialPath = '/search/javascript') => {
 };
 
 describe('subreddit form', () => {
-  it('initial setup: pass', () => {
-    setup();
+  it('shows the form title', () => {
+    setup('/search/javascript');
 
-    expect(true).toBeTruthy();
+    expect(screen.getByRole('heading', { name: /Find the best time for a subreddit/i })).toBeInTheDocument();
+  });
+
+  it('shows a text input prefix with r/', () => {
+    setup('/search/javascript');
+    const inputBox = screen.getByLabelText('r/');
+
+    expect(inputBox).toBeInTheDocument();
+    expect(inputBox.getAttribute('type')).toEqual('text');
+    // screen.debug(inputBox);
+  });
+
+  it('shows a submit button', () => {
+    setup('/search/javascript');
+    const submitButton = screen.getByRole('button', { name: /search/i });
+
+    expect(submitButton).toBeInTheDocument();
+  });
+
+  it('updates the URL on submit and initializes input value from URL', () => {
+    const { history } = setup('/search/javascript');
+    const inputBox = screen.getByLabelText('r/');
+    const submitButton = screen.getByRole('button', { name: /search/i });
+    const initialValue = history.location.pathname.split('/')[2];
+    // console.log(initialValue);
+
+    inputBox.value = '';
+    inputBox.value = initialValue;
+    expect(inputBox).toHaveValue(initialValue);
+
+    inputBox.value = '';
+    userEvent.type(inputBox, 'reactjs');
+
+    userEvent.click(submitButton);
+    history.push(`/search/${inputBox.value}`);
+    expect(history.location.pathname).toEqual('/search/reactjs');
   });
 });
--- src/page-search/Search.js
+++ src/page-search/Search.js
@@ -1,15 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import {
   Form, Input, Label, Button,
 } from './Search.style';
 
-function Search() {
+function Search(props) {
+  const [url, setUrl] = useState('javascript');
+  const baseUrl = window.location.origin;
+
+  function handleChange(evt) {
+    setUrl(evt.target.value);
+  }
+
+  // window.addEventListener('popstate', (evt) => {
+  //   if (!evt.explicitOriginalTarget.pathname) {
+
+  //   } else {
+  //     const defaultReddit = evt.explicitOriginalTarget.pathname.split('/')[2];
+  //     setUrl(defaultReddit);
+  //   }
+  // });
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    const nextUrl = `${baseUrl}/search/${url}`;
+    window.history.pushState({ state: 'search page' }, '', nextUrl);
+    // setUrl('');
+    // return
+  }
+
+  useEffect(() => {
+    const defaultReddit = window.location.pathname.split('/')[2];
+    setUrl(defaultReddit || 'javascript');
+  }, []);
+
   return (
-    <Form>
-      <Label>r/</Label>
-      <Input />
+    <Form onSubmit={handleSubmit}>
+      <Label>
+        r/
+        <Input
+          type="text"
+          value={url}
+          name="subreddit"
+          onChange={handleChange}
+        />
+      </Label>
       <Button>Search</Button>
+      <pre>
+        URL:
+        {JSON.stringify(props)}
+      </pre>
     </Form>
   );
 }
