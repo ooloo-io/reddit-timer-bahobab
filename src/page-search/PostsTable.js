2ccfbe7161a7b6cb11fcffe99d00e0290c25aa68
mono motapa <bahobab@hotmail.com>
2020-10-07 02:08:13 -0500

    heatmap: implemented hover, highlit and timezone

--- src/page-search/PostsTable.js
+++ src/page-search/PostsTable.js
@@ -4,37 +4,80 @@ import {
   WeekRow, Weekday, Cell, TimeFrame, HeatmapTable,
 } from './PostsTable.style';
 
+const mapWeekday = {
+  0: 'Sunday',
+  1: 'Monday',
+  2: 'Tuesday',
+  3: 'Wednesday',
+  4: 'Thursday',
+  5: 'Friday',
+  6: 'Saturday',
+};
+
+const cellBackgroundColorMap = {
+  0: '#e0e592',
+  1: '#aed396',
+  2: '#a9d194',
+  3: '#a0ce93',
+  4: '#99cd94',
+  5: '#8cc894',
+  6: '#5eb391',
+  7: '#5db492',
+  8: '#5cb391',
+  9: '#5aad8c',
+  10: '#3984a3',
+};
+
+// globals
+
+function CellWrapper({ postsInHour }) {
+  const numPosts = postsInHour.length;
+  const cellHighlight = '1px solid red';
+  const cellDefaultStyle = {
+    border: 'unset',
+    backgroundColor: `${getCellBGColor()}`,
+  };
+
+  const [cellStyle, setCellStyle] = useState(cellDefaultStyle);
+  const [isSelected, setIsSelected] = useState(false);
+
+  function getCellBGColor() {
+    return cellBackgroundColorMap[numPosts] || cellBackgroundColorMap[10];
+  }
+
+  function handleCellClick() {
+    setCellStyle(
+      { ...cellStyle, border: '1px solid red' },
+    );
+    setIsSelected(true);
+  }
+
+  function handleOnMouseEnter(event) {
+    if (event.target.style.border !== cellHighlight) {
+      event.target.style.border = cellHighlight;
+    }
+  }
+
+  function handleOnMouseLeave(event) {
+    if (!isSelected) {
+      event.target.style.border = 'none';
+    }
+  }
+
+  return (
+    <Cell
+      style={cellStyle}
+      onClick={handleCellClick}
+      onMouseEnter={handleOnMouseEnter}
+      onMouseLeave={handleOnMouseLeave}
+    >
+      {numPosts}
+    </Cell>
+  );
+}
+
 function PostsTable({ posts }) {
-  const mapWeekday = {
-    0: 'Sunday',
-    1: 'Monday',
-    2: 'Tuesday',
-    3: 'Wednesday',
-    4: 'Thursday',
-    5: 'Friday',
-    6: 'Saturday',
-  };
-
-  const cellBackgroundColorMap = {
-    0: '#e0e592',
-    1: '#aed396',
-    2: '#a9d194',
-    3: '#a0ce93',
-    4: '#99cd94',
-    5: '#8cc894',
-    6: '#5eb391',
-    7: '#5db492',
-    8: '#5cb391',
-    9: '#5aad8c',
-    10: '#3984a3',
-  };
-
-  const cellHighlightColor = 'red';
-
-  const [cellBgColor, setCellBgColor] = useState(cellBackgroundColorMap[0]);
-  const [cellHighlight, setCellHighlight] = useState('');
-
-  let myHeatmap;
+  // let myHeatmap;
 
   const postsPerHourPerDay = new Array(7)
     .fill([])
@@ -43,21 +86,21 @@ function PostsTable({ posts }) {
       .map(() => []));
 
   function getDayTimeFromTimeCreated(postCreatedAt) {
-    const day = new Date(postCreatedAt);
+    // https://duckduckgo.com/?q=javascript+date+always+point+to+epoch+time&t=ffab&atb=v196-1&ia=web
+    const d = new Date(0);
+    const day = new Date(d.setUTCSeconds(postCreatedAt));
     return {
       weekday: day.getDay(),
       timeOfDay: day.getHours(),
     };
   }
 
-  function buildPostsPerHourPerDayTable(subredditPosts, cb) {
+  function buildPostsPerHourPerDayTable(subredditPosts) {
     subredditPosts.forEach((post) => {
-      // console.log(post);
       const {
         id, author, created, title, url,
       } = post.data;
-      const { weekday, timeOfDay } = cb(created);
-      // console.log('>>>>>>>>>', weekday, timeOfDay);
+      const { weekday, timeOfDay } = getDayTimeFromTimeCreated(created);
       postsPerHourPerDay[Number(weekday)][Number(timeOfDay)].push({
         id, author, title, url, weekday, timeOfDay,
       });
@@ -65,46 +108,32 @@ function PostsTable({ posts }) {
   }
 
   function generateHeatmap() {
-    buildPostsPerHourPerDayTable(posts, getDayTimeFromTimeCreated);
-    // console.log('>>>>>>>>>', postsPerHourPerDay);
-    console.log('ttttttttttt', postsPerHourPerDay[0][22]);
+    buildPostsPerHourPerDayTable(posts);
     // try {
-    return postsPerHourPerDay.map((weekDay, weekDayIndex) => {
-      console.log('-------weekday', mapWeekday[weekDayIndex]);
-      return (
-        <WeekRow key={weekDayIndex}>
-          <Weekday>{mapWeekday[weekDayIndex]}</Weekday>
-          {weekDay.map((hour, hourIndex) => {
-            console.log('hour 22, posts', hourIndex, hour['22']);
-            return (
-              <Cell key={hourIndex}>{hour.length}</Cell>
-            );
-          })}
-        </WeekRow>
-      );
-    });
+    return postsPerHourPerDay.map((weekDay, weekDayIndex) => (
+      <WeekRow key={weekDayIndex}>
+        <Weekday>{mapWeekday[weekDayIndex]}</Weekday>
+        {weekDay.map((postsInHour, hourIndex) => (
+          <CellWrapper key={hourIndex} postsInHour={postsInHour} />
+        ))}
+      </WeekRow>
+    ));
     // } catch (error) {
     //   console.log('XXXXXXXXXXX', error);
     // }
-
-    // return myHeatmap;
   }
 
-  myHeatmap = generateHeatmap(); // .then((results) => results.map((hour) => hour));
-
-  console.log('**********', myHeatmap);
   const globalTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
   const localTimeZone = (new Date()).toTimeString().split('(')[1].replace(')', '');
   const userTimeZone = `${globalTimeZone} - ${localTimeZone}`;
 
   return (
     <div>
-      <h2>Heatmap Table</h2>
       <HeatmapTable>
         <TimeFrame><tr><th colSpan="24">Posts timeframe coming here</th></tr></TimeFrame>
-        <tbody>{myHeatmap}</tbody>
+        <tbody>{generateHeatmap()}</tbody>
       </HeatmapTable>
-      <div>{userTimeZone}</div>
+      <div style={{ textAlign: 'center' }}>{userTimeZone}</div>
     </div>
   );
 }
--- src/page-search/PostsTable.style.js
+++ src/page-search/PostsTable.style.js
@@ -2,16 +2,18 @@ import styled from 'styled-components';
 
 export const WeekRow = styled.tr`
   width: 100%;
+z-index: 10;
 `;
 
 export const Cell = styled.td`
+  /* background-color: ${(props) => props.bg}; */
   height: 40px;
   width: 40px;
   text-align: center;
-  :hover{
+  /* :hover{
     cursor: pointer;
     border: 1px solid red;
-  }
+  } */
 `;
 
 // merge these 2 td
